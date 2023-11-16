b0899acd37ff031eaa2a9c5a9975ca60b3497f9b
Routhred <romain.serpollet@gmail.com>
2023-11-16 19:49:30 +0100

    Ajout trie dossiers

--- sort_download.bat
+++ sort_download.bat
@@ -1,7 +1,12 @@
 set source=d:\TELECHARGEMENTS
 for /f "delims=|" %%a in ('dir /b %source%') do (
+	echo "%%a"
 	if "%%~xa" NEQ "" (
 		if not exist "%source%\%%~xa" mkdir "%source%\%%~xa"
 			move "%source%\%%a" "%source%\%%~xa\"
+	)
+	if "%%~xa" EQU "" (
+		if not exist "%source%\.dir" mkdir "%source%\.dir"
+			move "%source%\%%a" "%source%\.dir\"
 	)
 )
